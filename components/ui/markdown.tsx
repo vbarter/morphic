8f24a7faa354f07f3bfa99fb8ed5a0612accf27a
蔡俊杰 <caijunjie@bilibili.com>
2024-05-09 14:12:03 +0800

    优化代码高亮功能代码

--- components/ui/markdown.tsx
+++ components/ui/markdown.tsx
@@ -1,8 +1,7 @@
-import React, { FC, memo } from 'react';
+import React, { FC, memo, CSSProperties } from 'react';
 import ReactMarkdown from 'react-markdown';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { darcula } from 'react-syntax-highlighter/dist/esm/styles/prism';
-import {Option} from "commander";
 
 // 这里定义一个自定义的 code 样式对象，用来覆盖默认的 darcula 主题样式
 const customCodeStyle = {
@@ -19,31 +18,38 @@ const customCodeStyle = {
     },
 };
 
-// @ts-ignore
-export const MemoizedReactMarkdown: FC<Option> = memo(({ children, className, ...props }) => (
-    <ReactMarkdown
-        children={children}
-        className={className}
-        {...props}
-        components={{
-            code({ node, inline, className, children, ...props }) {
-                const match = /language-(\w+)/.exec(className || '');
-                return !inline && match ? (
-                    <SyntaxHighlighter
-                        style={customCodeStyle}
-                        language={match[1]}
-                        PreTag="div"
-                        children={String(children).replace(/\n$/, '')}
-                        {...props}
-                    />
-                ) : (
-                    <code className={className} {...props}>
-                        {children}
-                    </code>
-                );
-            },
-        }}
-    />
-), (prevProps, nextProps) =>
-    prevProps.children === nextProps.children &&
-    prevProps.className === nextProps.className);
+interface MemoizedReactMarkdownProps {
+    children: string;
+    className?: string;
+}
+
+export const MemoizedReactMarkdown: FC<MemoizedReactMarkdownProps> = memo(
+    ({ children, className, ...props }) => (
+        <ReactMarkdown
+            children={children}
+            className={className}
+            {...props}
+            components={{
+                code({ node, className, children }) {
+                    const match = /language-(\w+)/.exec(className || '');
+                    return match ? (
+                        <SyntaxHighlighter
+                            style={customCodeStyle}// You may need to assert the type if TypeScript can't infer it correctly
+                            language={match[1]}
+                            PreTag="div"
+                            children={String(children).replace(/\n$/, '')}
+                            {...props}
+                        />
+                    ) : (
+                        <code className={className} {...props}>
+                            {children}
+                        </code>
+                    );
+                },
+            }}
+        />
+    ),
+    (prevProps, nextProps) =>
+        prevProps.children === nextProps.children &&
+        prevProps.className === nextProps.className
+);
